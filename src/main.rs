4527d0b2f632e605ee857465bddd9bf2f6ed8da7
David Sullins <david.sullins@gmail.com>
2018-12-21 22:33:27 -0600

    Read a dictionary from stdin, print matches

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // read dictionary from stdin
+    let mut input = String::new();
+    std::io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read input");
+    let dict: HashSet<_> = input.split_whitespace().collect();
+
+    // find words
+    println!("Words that turn into new words when a Colemak typist types on a qwerty keyboard");
+    for (c, q) in find_matching_words(&dict) {
+        println!("{} -> {}", c, q);
+    }
 }
 
-fn colemak_as_qwerty(s: &str) -> String {
+fn find_matching_words(dictionary: &HashSet<&str>) -> HashMap<String, String> {
     let qwerty = "ertyuiopsdfgjkl;n";
     let colemak = "fpgjluy;rstdneiok";
     let qwerty_from_colemak = Translator::new(colemak, qwerty);
-    qwerty_from_colemak.translate(s)
+
+    let mut matches = HashMap::new();
+    for word in dictionary {
+        let qwerty = qwerty_from_colemak.translate(word);
+        if dictionary.contains::<str>(&qwerty) && qwerty != *word {
+            matches.insert(word.to_string(), qwerty.clone());
+        }
+    }
+
+    matches
 }
 
 struct Translator {
-    translation: std::collections::HashMap<char, char>,
+    translation: HashMap<char, char>,
 }
 
 impl Translator {
@@ -32,13 +56,37 @@ impl Translator {
     }
 }
 
-#[test]
-fn test_colemak_as_qwerty() {
-    let qwerty = "qwertyuiopasdfghjkl;zxcvbnm";
-    let colemak = "qwfpgjluy;arstdhneiozxcvbkm";
-    assert_eq!("", colemak_as_qwerty(""));
-    assert_eq!("a", colemak_as_qwerty("a"));
-    assert_eq!("j", colemak_as_qwerty("n"));
-    assert_eq!(qwerty, colemak_as_qwerty(colemak));
-    assert_eq!("fork", colemak_as_qwerty("type"));
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    fn colemak_as_qwerty(s: &str) -> String {
+        let qwerty = "ertyuiopsdfgjkl;n";
+        let colemak = "fpgjluy;rstdneiok";
+        let qwerty_from_colemak = Translator::new(colemak, qwerty);
+        qwerty_from_colemak.translate(s)
+    }
+
+    #[test]
+    fn test_colemak_as_qwerty() {
+        let qwerty = "qwertyuiopasdfghjkl;zxcvbnm";
+        let colemak = "qwfpgjluy;arstdhneiozxcvbkm";
+        assert_eq!("", colemak_as_qwerty(""));
+        assert_eq!("a", colemak_as_qwerty("a"));
+        assert_eq!("j", colemak_as_qwerty("n"));
+        assert_eq!(qwerty, colemak_as_qwerty(colemak));
+        assert_eq!("fork", colemak_as_qwerty("type"));
+    }
+
+    #[test]
+    fn test_find_matching_words() {
+        let mut dict: HashSet<&str> = HashSet::new();
+        dict.insert("a");
+        dict.insert("I");
+        dict.insert("type");
+        dict.insert("fork");
+        let mut expected = HashMap::new();
+        expected.insert("type".to_string(), "fork".to_string());
+        assert_eq!(expected, find_matching_words(&dict));
+    }
 }
